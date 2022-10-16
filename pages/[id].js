c1f66fada7631c8d509f06dd62fdfe1791135a68
Shin-sibainu <shincode0712@gmail.com>
2022-10-17 07:57:21 +0900

    done

--- pages/[id].js
+++ pages/[id].js
@@ -232,6 +232,6 @@ export const getStaticProps = async (context) => {
       page,
       blocks: blocksWithChildren,
     },
-    revalidate: 1,
+    revalidate: 1, //ISR...前回から何秒以内のアクセスを無視するか指定します。
   };
 };
